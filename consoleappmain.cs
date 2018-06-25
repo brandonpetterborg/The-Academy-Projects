98f6baebd10ec2e4faa8e7df806a5eb1172df46d
Brandon <pettbra2@gmail.com>
2018-06-25 10:11:05 -0600

    consoleappmain

---
+++ consoleappmain.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+namespace consoleAppMain
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, We're \"accounting\" on you!");
+            Console.Read();
+           
+        }
+    }
+}

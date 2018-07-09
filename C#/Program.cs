375613f16936215ae1175136f5d80fe0c562c328
Brandon <pettbra2@gmail.com>
2018-07-09 14:41:37 -0600

    ipdate

--- C#/Program.cs
+++ C#/Program.cs
@@ -10,7 +10,7 @@ namespace CallingMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose a number.");
+            Console.WriteLine("Choose a number. We will add 2, multiply by 2, then divide by 2");
             int number1 = Convert.ToInt32(Console.ReadLine());
             //int number1 = Convert.ToInt32(Console.ReadLine());
 

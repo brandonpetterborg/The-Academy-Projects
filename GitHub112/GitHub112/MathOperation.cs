23fbbfb448735589d2046480ef409b9653cf66ba
Brandon <pettbra2@gmail.com>
2018-07-25 08:29:26 -0600

    update

---
+++ GitHub112/GitHub112/MathOperation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub112
+{
+    public class MathOperation
+    {
+
+
+        //public void Addition(int number1)
+        //{
+        //    Console.WriteLine(number1 + 2);
+        //    Console.ReadLine();
+            
+        //}
+        //public void Addition(decimal number2)
+        //{
+        //    Console.WriteLine(number2 + 1);
+        //    Console.ReadLine();
+          
+        //}
+        public void Addition(int number3)
+            {
+            Console.WriteLine(number3 + 5);
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ GitHub112/GitHub112/Program.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub112
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            //Console.WriteLine("input integer");
+            //int userInput1 = Convert.ToInt32(Console.ReadLine());
+            //MathOperation mathOperation2 = new MathOperation();
+            //mathOperation2.Addition(userInput1);
+            //Console.ReadLine();
+
+            //    try
+            //    {
+            //        Console.WriteLine("input integer");
+            //        decimal userInput = Convert.ToDecimal(Console.ReadLine());
+            //        MathOperation mathOperation1 = new MathOperation();
+            //        mathOperation1.Addition(userInput);
+            //        Console.ReadLine();
+            //}
+            //    catch (FormatException ex)
+            //    {
+            //        Console.WriteLine(ex);
+            //    }
+            //    catch (InvalidCastException ex)
+            //    {
+            //        Console.WriteLine(ex);
+            //    }
+
+            Console.WriteLine("input number");
+            string userInputString = Console.ReadLine();
+            int userInput2 = Convert.ToInt32(userInputString);
+            int.TryParse(userInputString, out userInput2);
+            MathOperation mathOperation3 = new MathOperation();
+            mathOperation3.Addition(userInput2);
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}

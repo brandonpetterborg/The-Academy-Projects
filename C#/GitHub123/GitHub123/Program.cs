bb5e572d24754663d9213d226bdad79356c930b9
Brandon <pettbra2@gmail.com>
2018-07-25 11:47:19 -0600

    update

---
+++ C#/GitHub123/GitHub123/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Sample";
+            employee1.LastName = "Student";
+            employee1.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}

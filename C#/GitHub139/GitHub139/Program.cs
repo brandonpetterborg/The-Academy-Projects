99e4191466e128c6579490ebbe2a6bc2f0d09b23
Brandon <pettbra2@gmail.com>
2018-08-07 14:01:07 -0600

    update

---
+++ C#/GitHub139/GitHub139/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub139
+{
+    public class Author
+    {
+        public static Main();
+
+
+        private string name;
+        private short id;
+        
+        List<Author> AuthorList = new List<Author>();
+        public Author(string name, short id)
+        {
+            this.name = name;
+            this.id = id;
+
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public short Age
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        
+        
+       
+        
+    }
+}
+    
+
